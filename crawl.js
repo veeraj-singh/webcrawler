882356dc411de148e405ec14218a09fb6b71b64e
veeraj-singh <veerbachche82@gmail.com>
2023-09-09 16:01:42 +0530

    Page crawling function added

--- crawl.js
+++ crawl.js
@@ -22,4 +22,41 @@ function getURLsFromHTML(htmlbody,baseurl){
     return urls
 }
 
-module.exports={normalizeURL,getURLsFromHTML}
+async function CrawlPage(baseurl,currenturl,pages){
+    const baseurl_obj=new URL(baseurl)
+    const currenturl_obj=new URL(currenturl)
+    if(baseurl_obj.hostname!==currenturl_obj.hostname){
+        return pages
+    }
+
+    const norm_url=normalizeURL(currenturl)
+    if(pages[norm_url]>0){
+        pages[norm_url]++
+        return pages
+    }
+    baseurl!==currenturl?pages[norm_url]=1:pages[norm_url]=0
+    console.log(`Crawling currently on page : ${currenturl}`)
+    try{
+        const response= await fetch(currenturl)
+        if(response.status>399){
+            console.log(`Failed to fetch the URL with status code: ${response.status}`)
+            return pages
+        }
+        if(!response.headers.get('content-type').includes('text/html')){
+            console.log(`Failed to fetch URL with content type: ${response.headers.get('content-type')}`)
+            return pages
+        }
+    const htmlbody= await response.text()
+    const urls=getURLsFromHTML(htmlbody,baseurl)
+    for(const url of urls){
+        pages=await CrawlPage(baseurl,url,pages)
+    }
+
+    }catch(err){
+        console.log(`Error :${err.message} on page:${baseurl}`)
+    }
+    return pages
+    
+}
+
+module.exports={normalizeURL,getURLsFromHTML,CrawlPage}
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function main() {
+const {CrawlPage}= require('./crawl.js')
+async function main() {
     if(process.argv.length>3){
         console.log("Error : Provide a single url please")
         process.exit(1)
@@ -8,7 +9,12 @@ function main() {
         process.exit(1)
     }
     else{
-        console.log(`Crawler is satrting at BASEURL : ${process.argv[2]}`)
+        console.log(`Crawler is starting at BASEURL : ${process.argv[2]}`)
+    }
+    const baseurl=process.argv[2]
+    const pages=await CrawlPage(baseurl,baseurl,{})
+    for(const page of Object.entries(pages)){
+        console.log(page)
     }
 }
 main()
