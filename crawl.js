efbcd3341d2408cf57f3e753ac21fe124cb18d74
veeraj-singh <veerbachche82@gmail.com>
2023-09-09 12:40:53 +0530

    Getting url links from page completed

--- crawl.js
+++ crawl.js
@@ -1,3 +1,4 @@
+const {JSDOM}= require('jsdom')
 function normalizeURL(url){
     if(url.slice(-1)==='/'){
         url=url.slice(0,-1)
@@ -7,4 +8,18 @@ function normalizeURL(url){
     return norm_url
 }
 
-module.exports={normalizeURL}
+function getURLsFromHTML(htmlbody,baseurl){
+    const {document}=new JSDOM(htmlbody).window
+    const list=document.querySelectorAll('a')
+    let urls=[]
+    list.forEach((item)=>{
+        let url=item.href
+        if(url[0]==='/'){
+            url=`${baseurl}${url}`
+        }
+        urls.push(url)
+    })
+    return urls
+}
+
+module.exports={normalizeURL,getURLsFromHTML}
--- crawl.test.js
+++ crawl.test.js
@@ -1,21 +1,51 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL,getURLsFromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 
-test('normalized url',()=>{
+test('normalized https url',()=>{
     const input="https://xyz.dev/path/"
     const result="xyz.dev/path"
     expect(normalizeURL(input)).toBe(result)
 }
 )
-test('normalized url',()=>{
+test('normalized capital url',()=>{
     const input="http://XYZ.DEV/path/"
     const result="xyz.dev/path"
     expect(normalizeURL(input)).toBe(result)
 }
 )
-test('normalized url',()=>{
+test('normalized http url',()=>{
     const input="http://xyz.dev/path"
     const result="xyz.dev/path"
     expect(normalizeURL(input)).toBe(result)
+}
+)
+
+test('list of all urls from html-page',()=>{
+    const baseurl='https://xyz.dev'
+    const input=
+        `<!DOCTYPE html>
+    <html lang="en">
+    <body>
+        <a href="https://xyz.dev/path"> url for next page</a>
+        <a href="https://abc.dev/path"> url for next page</a>
+    </body>
+    </html>`
+    const result=["https://xyz.dev/path","https://abc.dev/path"]
+    expect(getURLsFromHTML(input,baseurl)).toEqual(result)
+}
+)
+
+test('list of all relative urls to absolute urls from html-page',()=>{
+    const baseurl='https://xyz.dev'
+    const input=
+        `<!DOCTYPE html>
+    <html lang="en">
+    <body>
+        <a href="/path"> url for next page</a>
+        <a href="/blog"> url for next page</a>
+    </body>
+    </html>`
+    const result=["https://xyz.dev/path","https://xyz.dev/blog"]
+    expect(getURLsFromHTML(input,baseurl)).toEqual(result)
 }
 )
