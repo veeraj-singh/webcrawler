fb5a20b24602054eb47c25109059013ea038777a
veeraj-singh <veerbachche82@gmail.com>
2023-09-09 12:53:48 +0530

    check om CMD arguments done

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+function main() {
+    if(process.argv.length>3){
+        console.log("Error : Provide a single url please")
+        process.exit(1)
+    }
+    if(process.argv.length<3){
+        console.log("Error : Give url please")
+        process.exit(1)
+    }
+    else{
+        console.log(`Crawler is satrting at BASEURL : ${process.argv[2]}`)
+    }
+}
+main()
